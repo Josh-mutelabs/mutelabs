4349fbf56ada89a7a7b633aa69580194a1b08019
josh-mutelabs <josh@mutelabs.io>
2020-11-23 23:44:07 +1100

    LandingPage Temp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import logo from './assets/images/MuteLabsIcon_Pink.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header padding">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          MuteLabs
+        <p className="mutelabs-label">
+          <strong>mute</strong>labs.io
         </p>
       </header>
     </div>
